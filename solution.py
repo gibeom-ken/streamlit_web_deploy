a442c66c26a15f0bd1a17399296dd023ede0ddb7
gibeom-ken <158978093+gibeom-ken@users.noreply.github.com>
2024-02-05 11:48:03 +0900

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,14 @@
+import pygwalker as pyg
+import pandas as pd
+import streamlit.components.v1 as components
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="API데이터솔루션을 활용한 대시보드 만들기",
+    layout="wide"
+)
+
+# Add Title
+st.title("API데이터솔루션을 활용한 대시보드 만들기")
+ 
